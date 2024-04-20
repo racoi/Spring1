1af7352dd557ed71d42d7da41d9b5fd68c7605e0
racoi <87438305+racoi@users.noreply.github.com>
2024-04-20 15:14:39 +0900

    @Qualifier 적용, mainDiscountPolicy 애노테이션 생성, List&Map 테스트

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
+        basePackages = "hello.core",
         // 기존 예제 코드 유지하기 위해
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
